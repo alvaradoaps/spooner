66ce30f40364a5159352ec48df952af1b33cca5f
Josias Alvarado <31009853+alvaradoaps@users.noreply.github.com>
2018-03-06 21:57:30 -0600

    Create chooser.py

---
+++ chooser.py
@@ -0,0 +1,19 @@
+# Functions for generating rules -- XSLT and TC-XML
+
+import datetime
+
+TIME_STAMP = str(datetime.datetime.utcnow())
+FILE_NAME = 'Rules-' + TIME_STAMP.replace(' ','T' ).split('.')[0] + '.xml' 
+
+def build_xslt(xpath, to_search, to_assign, otherwise_val):
+   with open(FILE_NAME, 'a') as data:
+        data.write('''<xsl:when test=\"contains({},\'{}\')\"
+                    >{}</xsl:when>'''.format(xpath, to_search, to_assign))
+        data.write('<xsl:otherwise>{}</xsl:otherwise>'.format(otherwise_val))
+
+def build_tc(to_search, to_assign):
+    with open(FILE_NAME, 'a') as data:
+        data.write('''<Transformation type=\"ContainsText\">
+                        <TextToSearchFor><![CDATA[{}]]></TextToSearchFor>
+                        <TrueValue><![CDATA[{}]]</TrueValue>
+                      </Transformation>'''.format(to_search, to_assign))
