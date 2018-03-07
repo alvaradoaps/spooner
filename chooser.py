c0e781b4e50922b525065f3d875d10d103564e76
Josias Alvarado <31009853+alvaradoaps@users.noreply.github.com>
2018-03-06 22:00:22 -0600

    Update chooser.py

--- chooser.py
+++ chooser.py
@@ -1,6 +1,7 @@
 # Functions for generating rules -- XSLT and TC-XML
 
 import datetime
+from pandas import read_excel
 
 TIME_STAMP = str(datetime.datetime.utcnow())
 FILE_NAME = 'Rules-' + TIME_STAMP.replace(' ','T' ).split('.')[0] + '.xml' 
@@ -17,3 +18,9 @@ def build_tc(to_search, to_assign):
                         <TextToSearchFor><![CDATA[{}]]></TextToSearchFor>
                         <TrueValue><![CDATA[{}]]</TrueValue>
                       </Transformation>'''.format(to_search, to_assign))
+
+def allocData(xlsxFile, columnOneName, columnTwoName):
+   data = read_excel(xlsxFile)
+   columnOne = data[columnOneName]
+   columnTwo = data[columnTwoName]
+   return zip(columnOne, columnTwo)
