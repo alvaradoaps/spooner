a39ad0fa0beb14abc3418a8bbd693cc655c6122f
Josias Alvarado <31009853+alvaradoaps@users.noreply.github.com>
2018-03-07 08:31:40 -0600

    Update chooser.py

--- chooser.py
+++ chooser.py
@@ -3,16 +3,20 @@
 import datetime
 from pandas import read_excel
 
-TIME_STAMP = str(datetime.datetime.utcnow())
+TIME_STAMP = str(datetime.datetime.utcnow()) 
 FILE_NAME = 'Rules-' + TIME_STAMP.replace(' ','T' ).split('.')[0] + '.xml' 
+# FILE_NAME global creates unique file name to avoid appending to a existing file 
+# when not intended.
 
 def build_xslt(xpath, to_search, to_assign, otherwise_val):
-   with open(FILE_NAME, 'a') as data:
+    '''This function builds XSLT whens.'''
+    with open(FILE_NAME, 'a') as data:
         data.write('''<xsl:when test=\"contains({},\'{}\')\"
                     >{}</xsl:when>'''.format(xpath, to_search, to_assign))
         data.write('<xsl:otherwise>{}</xsl:otherwise>'.format(otherwise_val))
 
 def build_tc(to_search, to_assign):
+    '''This function builds TC-XML transformations.'''
     with open(FILE_NAME, 'a') as data:
         data.write('''<Transformation type=\"ContainsText\">
                         <TextToSearchFor><![CDATA[{}]]></TextToSearchFor>
@@ -20,7 +24,8 @@ def build_tc(to_search, to_assign):
                       </Transformation>'''.format(to_search, to_assign))
 
 def alloc_data(xlsx_file, column_one_name, column_two_name):
-   data = read_excel(xlsx_file)
-   column_one = data[column_one_name]
-   column_two = data[column_two_name]
-   return zip(column_one, column_two)
+    '''Allocates columns into a zip object.'''
+    data = read_excel(xlsx_file)
+    column_one = data[column_one_name]
+    column_two = data[column_two_name]
+    return zip(column_one, column_two)
